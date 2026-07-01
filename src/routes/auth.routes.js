ec05db6aba59541a26e4d1a2e086c26de9f06a24
SebsNich <juan.nichollsriv@ug.edu.ec>
2026-07-01 11:27:04 -0500

    fix: agregar multer a ruta editar-perfil para procesar foto

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const { verificarToken } = require('../middleware/auth.middleware')
 const { registrar, login, obtenerPerfil, editarPerfil, cambiarPassword, cambiarEmail } = require('../controllers/auth.controller')
+const upload = require('../lib/multer')
 
 router.post('/registrar', registrar)
 router.post('/login', login)
 router.get('/perfil', verificarToken, obtenerPerfil)
-router.put('/editar-perfil', verificarToken, editarPerfil)
+router.put('/editar-perfil', verificarToken, upload.single('foto'), editarPerfil)
 router.put('/cambiar-password', verificarToken, cambiarPassword)
 router.put('/cambiar-email', verificarToken, cambiarEmail)
 
